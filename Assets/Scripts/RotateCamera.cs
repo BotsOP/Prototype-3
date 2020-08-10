6890cdaa6165323db58bcba79868c73098b5b0d2
DESKTOP-O1U6U1E\prutm <prutmaild@gmail.com>
2020-08-10 12:45:50 +0200

    First commit

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontaleInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontaleInput * rotationSpeed * Time.deltaTime);
+    }
+}
